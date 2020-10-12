cc444731f9c38499fa887def53b782bc6caec8ab
Mannan Bansal <mannan_bansal@yahoo.com>
2020-10-12 13:36:15 +0530

    Added code for transmission and imported necessary modules

--- HazeRemoval/dehaze.py
+++ HazeRemoval/dehaze.py
@@ -1,18 +1,34 @@
+import numpy as np
+from cv2.ximgproc import guidedFilter
+
 class dehaze:
+
+    omega = 0.95
+    top_p = 0.001
+    patchSize = 15
+    thresholdT = 0.1
+
+
     def __init__(self, image):
         self.image = image
     
     def getDarkChannel(self):
-        print("Dark Channel Code Here")
+        print(self.image)
     
     def getAtmosphericLight(self):
         print("Code Here")
+        return 1
     
     def getRawTransmission(self):
-        print("Code Here")
+        atmLight = self.getAtmosphericLight()
+        transmission = 1 - self.omega*(self.getDarkChannel()/atmLight)
+        return transmission
+
 
     def getRefineTransmission(self):
-        print("Code Here")
+        rawT = self.getRawTransmission()
+        refinedT = guidedFilter(self.image.astype(np.float32), rawT.astype(np.float32), 50, 1e-4)
+        return refinedT
 
     def getRadiance(self):
         print("Code Here")
