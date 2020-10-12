d981741d8d4df8800f2aead7ad7f474057d11375
Mannan Bansal <mannan_bansal@yahoo.com>
2020-10-12 12:57:29 +0530

    Added Dehaze Class Structure

---
+++ HazeRemoval/dehaze.py
@@ -0,0 +1,24 @@
+class dehaze:
+    def __init__(self, image):
+        self.image = image
+    
+    def getDarkChannel(self):
+        print("Dark Channel Code Here")
+    
+    def getAtmosphericLight(self):
+        print("Code Here")
+    
+    def getRawTransmission(self):
+        print("Code Here")
+
+    def getRefineTransmission(self):
+        print("Code Here")
+
+    def getRadiance(self):
+        print("Code Here")
+
+    def equalizeBrightness(self):
+        print("Code Here")
+
+    def getDehazedImage(self):
+        print("Code Here")
